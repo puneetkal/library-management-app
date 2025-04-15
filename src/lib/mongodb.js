3663e03fd7a3ee921a6e12056f4002d0f31575f6
puneetkal <puneetka181@gmail.com>
2025-04-15 15:33:55 +0530

    env

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 // const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/auth-db';
-const MONGODB_URI = 'mongodb+srv://puneet:puneet@cluster0.phvpl.mongodb.net/';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable');
