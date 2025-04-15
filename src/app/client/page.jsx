2b7b52340714a1eb62a131c310d3e3d3ba4c8651
puneetkal <puneetka181@gmail.com>
2025-04-15 15:42:15 +0530

    useeffect

--- src/app/client/page.jsx
+++ src/app/client/page.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useRouter } from 'next/navigation'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const page = () => {
   const router = useRouter()
